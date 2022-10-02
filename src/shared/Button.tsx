74bc4de2f324fedb2f982f666bbfe9d1f680405c
fpatr <fpatr@qq.com>
2022-10-02 18:54:47 +0800

    创建 StartPage 和 Button

--- src/shared/Button.tsx
+++ src/shared/Button.tsx
@@ -1,8 +1,13 @@
 import { defineComponent } from "vue";
 import s from './Button.module.scss'
-export const Button = defineComponent({
+
+interface Props{
+    onClick: (e: MouseEvent) => void
+}
+
+export const Button = defineComponent<Props>({
     //如果不想让button自动继承调用者的class和onClick，而是在Button页面完成功能，可以采用以下代码：
-    //inheritAttrs: false,
+    //inheritAttrs: false,  //如果改为false，请参照第9课视频 创建 Button 组件 的24:30~29:00
     setup: (props, context) => {
         return () => (
             <button class={s.button}>

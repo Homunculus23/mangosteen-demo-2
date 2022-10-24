02648f93ff0a5aa91e6f360c76f42e727ff34401
fpatr <fpatr@qq.com>
2022-10-24 19:51:40 +0800

    修复两个Button的CSS变量冲突的BUG

--- src/shared/Button.tsx
+++ src/shared/Button.tsx
@@ -1,10 +1,8 @@
 import { defineComponent } from "vue";
-import s from './Button.module.scss'
-
+import s from './Button.module.scss';
 interface Props{
     onClick?: (e: MouseEvent) => void   //如果onClick后面不加问号，调用Button就必须传onClick
 }
-
 export const Button = defineComponent<Props>({
     //如果不想让button自动继承调用者的class和onClick，而是在Button页面完成功能，可以采用以下代码：
     //inheritAttrs: false,  //如果改为false，请参照第9课视频 创建 Button 组件 的24:30~29:00

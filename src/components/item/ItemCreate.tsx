3d7f4b1870d033267f3e629935e98d9b6a0b9af7
fpatr <fpatr@qq.com>
2022-10-28 19:08:04 +0800

    完成记一笔页面样式

--- src/components/item/ItemCreate.tsx
+++ src/components/item/ItemCreate.tsx
@@ -38,19 +38,113 @@ export const ItemCreate = defineComponent({
             { id: 4, name: '投资', sign: '￥', kind: 'income'},
             { id: 5, name: '利息', sign: '￥', kind: 'income'},
             { id: 6, name: '工资', sign: '￥', kind: 'income'},
+            { id: 4, name: '投资', sign: '￥', kind: 'income'},
+            { id: 5, name: '利息', sign: '￥', kind: 'income'},
+            { id: 6, name: '工资', sign: '￥', kind: 'income'},
+            { id: 4, name: '投资', sign: '￥', kind: 'income'},
+            { id: 5, name: '利息', sign: '￥', kind: 'income'},
+            { id: 6, name: '工资', sign: '￥', kind: 'income'},
+            { id: 4, name: '投资', sign: '￥', kind: 'income'},
+            { id: 5, name: '利息', sign: '￥', kind: 'income'},
+            { id: 6, name: '工资', sign: '￥', kind: 'income'},
+            { id: 4, name: '投资', sign: '￥', kind: 'income'},
+            { id: 5, name: '利息', sign: '￥', kind: 'income'},
+            { id: 6, name: '工资', sign: '￥', kind: 'income'},
+            { id: 4, name: '投资', sign: '￥', kind: 'income'},
+            { id: 5, name: '利息', sign: '￥', kind: 'income'},
+            { id: 6, name: '工资', sign: '￥', kind: 'income'},
+            { id: 4, name: '投资', sign: '￥', kind: 'income'},
+            { id: 5, name: '利息', sign: '￥', kind: 'income'},
+            { id: 6, name: '工资', sign: '￥', kind: 'income'},
+            { id: 4, name: '投资', sign: '￥', kind: 'income'},
+            { id: 5, name: '利息', sign: '￥', kind: 'income'},
+            { id: 6, name: '工资', sign: '￥', kind: 'income'},
+            { id: 4, name: '投资', sign: '￥', kind: 'income'},
+            { id: 5, name: '利息', sign: '￥', kind: 'income'},
+            { id: 6, name: '工资', sign: '￥', kind: 'income'},
+            { id: 4, name: '投资', sign: '￥', kind: 'income'},
+            { id: 5, name: '利息', sign: '￥', kind: 'income'},
+            { id: 6, name: '工资', sign: '￥', kind: 'income'},
+            { id: 4, name: '投资', sign: '￥', kind: 'income'},
+            { id: 5, name: '利息', sign: '￥', kind: 'income'},
+            { id: 6, name: '工资', sign: '￥', kind: 'income'},
+            { id: 4, name: '投资', sign: '￥', kind: 'income'},
+            { id: 5, name: '利息', sign: '￥', kind: 'income'},
+            { id: 6, name: '工资', sign: '￥', kind: 'income'},
+            { id: 4, name: '投资', sign: '￥', kind: 'income'},
+            { id: 5, name: '利息', sign: '￥', kind: 'income'},
+            { id: 6, name: '工资', sign: '￥', kind: 'income'},
+            { id: 4, name: '投资', sign: '￥', kind: 'income'},
+            { id: 5, name: '利息', sign: '￥', kind: 'income'},
+            { id: 6, name: '工资', sign: '￥', kind: 'income'},
+            { id: 4, name: '投资', sign: '￥', kind: 'income'},
+            { id: 5, name: '利息', sign: '￥', kind: 'income'},
+            { id: 6, name: '工资', sign: '￥', kind: 'income'},
+            { id: 4, name: '投资', sign: '￥', kind: 'income'},
+            { id: 5, name: '利息', sign: '￥', kind: 'income'},
+            { id: 6, name: '工资', sign: '￥', kind: 'income'},
+            { id: 4, name: '投资', sign: '￥', kind: 'income'},
+            { id: 5, name: '利息', sign: '￥', kind: 'income'},
+            { id: 6, name: '工资', sign: '￥', kind: 'income'},
+            { id: 4, name: '投资', sign: '￥', kind: 'income'},
+            { id: 5, name: '利息', sign: '￥', kind: 'income'},
+            { id: 6, name: '工资', sign: '￥', kind: 'income'},
+            { id: 4, name: '投资', sign: '￥', kind: 'income'},
+            { id: 5, name: '利息', sign: '￥', kind: 'income'},
+            { id: 6, name: '工资', sign: '￥', kind: 'income'},
+            { id: 4, name: '投资', sign: '￥', kind: 'income'},
+            { id: 5, name: '利息', sign: '￥', kind: 'income'},
+            { id: 6, name: '工资', sign: '￥', kind: 'income'},
+            { id: 4, name: '投资', sign: '￥', kind: 'income'},
+            { id: 5, name: '利息', sign: '￥', kind: 'income'},
+            { id: 6, name: '工资', sign: '￥', kind: 'income'},
+            { id: 4, name: '投资', sign: '￥', kind: 'income'},
+            { id: 5, name: '利息', sign: '￥', kind: 'income'},
+            { id: 6, name: '工资', sign: '￥', kind: 'income'},
+            { id: 4, name: '投资', sign: '￥', kind: 'income'},
+            { id: 5, name: '利息', sign: '￥', kind: 'income'},
+            { id: 6, name: '工资', sign: '￥', kind: 'income'},
+            { id: 4, name: '投资', sign: '￥', kind: 'income'},
+            { id: 5, name: '利息', sign: '￥', kind: 'income'},
+            { id: 6, name: '工资', sign: '￥', kind: 'income'},
+            { id: 4, name: '投资', sign: '￥', kind: 'income'},
+            { id: 5, name: '利息', sign: '￥', kind: 'income'},
+            { id: 6, name: '工资', sign: '￥', kind: 'income'},
+            { id: 4, name: '投资', sign: '￥', kind: 'income'},
+            { id: 5, name: '利息', sign: '￥', kind: 'income'},
+            { id: 6, name: '工资', sign: '￥', kind: 'income'},
         ])
         return () => (
             //注意：<MainLayout> 和上下花括号之间不能有空格，否则花括号中的内容将会被视为数组，无法变成插槽。
-            <MainLayout>{
-                {
-                    icon: () => <Icon name="left" class={s.navIcon}/>,
-                    title: () => '记一笔',
-                    default: () => <>
-                    <div class={s.wrapper}>
-                            {/* 网页第一次渲染时就将refKind.value赋予 update: 的 selected。
-                            双向绑定事件，当回调参数与refKind.value不同时，将获取的回调参数赋予refKind.value，并将refKind.value赋予 update: 的 selected，该行为将使Tabs重新渲染。 */}
-                            <Tabs v-model:selected={refKind.value} class={s.tabs} >
-                                <Tab name="支出" class={s.tags_wrapper}>
+            <MainLayout class={s.layout}>{{
+                icon: () => <Icon name="left" class={s.navIcon}/>,
+                title: () => '记一笔',
+                default: () => <>
+                <div class={s.wrapper}>
+                        {/* 网页第一次渲染时就将refKind.value赋予 update: 的 selected。
+                        双向绑定事件，当回调参数与refKind.value不同时，将获取的回调参数赋予refKind.value，并将refKind.value赋予 update: 的 selected，该行为将使Tabs重新渲染。 */}
+                        <Tabs v-model:selected={refKind.value} class={s.tabs} >
+                            <Tab name="支出" class={s.tags_wrapper}>
+                            <div class={s.tag}>
+                                <div class={s.sign}>
+                                    <Icon name="add" class={s.createTag} />
+                                </div>
+                                <div class={s.name}>
+                                    新增
+                                </div>
+                            </div>
+                                {refExpensesTags.value.map(tag =>
+                                    <div class={[s.tag, s.selected]}>
+                                        <div class={s.sign}>
+                                            {tag.sign}
+                                        </div>
+                                        <div class={s.name}>
+                                            {tag.name}
+                                        </div>
+                                    </div>
+                                )}
+                            </Tab>
+                            <Tab name="收入" class={s.tags_wrapper}>
                                 <div class={s.tag}>
                                     <div class={s.sign}>
                                         <Icon name="add" class={s.createTag} />
@@ -59,46 +153,25 @@ export const ItemCreate = defineComponent({
                                         新增
                                     </div>
                                 </div>
-                                    {refExpensesTags.value.map(tag =>
-                                        <div class={[s.tag, s.selected]}>
-                                            <div class={s.sign}>
-                                                {tag.sign}
-                                            </div>
-                                            <div class={s.name}>
-                                                {tag.name}
-                                            </div>
-                                        </div>
-                                    )}
-                                </Tab>
-                                <Tab name="收入" class={s.tags_wrapper}>
-                                    <div class={s.tag}>
+                                {refIncomeTags.value.map(tag =>
+                                    <div class={[s.tag, s.selected]}>
                                         <div class={s.sign}>
-                                            <Icon name="add" class={s.createTag} />
+                                            {tag.sign}
                                         </div>
                                         <div class={s.name}>
-                                            新增
+                                            {tag.name}
                                         </div>
                                     </div>
-                                    {refIncomeTags.value.map(tag =>
-                                        <div class={[s.tag, s.selected]}>
-                                            <div class={s.sign}>
-                                                {tag.sign}
-                                            </div>
-                                            <div class={s.name}>
-                                                {tag.name}
-                                            </div>
-                                        </div>
-                                    )}
-                                </Tab>
-                            </Tabs>
-                            {/* 用div为InputPad作定位 */}
-                            <div class={s.inputPad_wrapper}>
-                                <InputPad></InputPad>
-                            </div>
+                                )}
+                            </Tab>
+                        </Tabs>
+                        {/* 用div为InputPad作定位 */}
+                        <div class={s.inputPad_wrapper}>
+                            <InputPad/>
                         </div>
-                    </>
-                }
-            }</MainLayout>
+                    </div>
+                </>
+            }}</MainLayout>
         )
     }
 })

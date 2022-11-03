b58566f5decedaeb44077a027521e193ecc2e949
fpatr <fpatr@qq.com>
2022-11-03 15:37:24 +0800

    准备封装 Form 组件

--- src/components/item/ItemList.tsx
+++ src/components/item/ItemList.tsx
@@ -1,38 +1,69 @@
-import { defineComponent, PropType, ref } from "vue";
+import { Overlay } from "vant";
+import { defineComponent, PropType, reactive, ref, watchEffect } from "vue";
 import { MainLayout } from "../../layouts/MainLayout";
 import { Icon } from "../../shared/Icon";
 import { Tab, Tabs } from "../../shared/Tabs";
+import { Time } from "../../shared/time";
 import s from './ItemList.module.scss';
+import { ItemSummary } from "./ItemSummary";
 export const ItemList = defineComponent({
-    props:{
-        name: {
-            type: String as PropType<string>
-        }
-    },
     setup: (props, context) => {
         const refSelected = ref('本月')
+        const time = new Time()
+        //自定义
+        const customTime = reactive({start: new Time().format(), end: new Time().format()})
+        const timeList = [
+          //本月
+          {start: time.firstDayOfMonth(), end: time.lastDayOfMonth()},
+          //下月
+          {start: time.add(-1, 'month').firstDayOfMonth(), end: time.add(-1, 'month').lastDayOfMonth()},
+          //今年
+          {start: time.firstDayOfYear(), end: time.lastDayOfYear()},
+        ]
+        watchEffect(() => {
+          if (refSelected.value === '自定义时间') {
+            refOverlayVisible.value = true
+          }
+        })
+        const refOverlayVisible = ref(false)
+        const onSubmitCustomTime = (e: Event) => {
+          e.preventDefault()
+          refOverlayVisible.value = false
+        }
         return () => (
           <MainLayout>{
             {
               title: () => '山竹记账',
               icon: () => <Icon name="menu" />,
-              default: () => (
-                //classPrefix={'customTabs'}
+              default: () => (<>
                 <Tabs classPrefix={'customTabs'} v-model:selected={refSelected.value}>
                   <Tab name="本月">
-                    list 1
+                    <ItemSummary startDate={timeList[0].start.format()} endDate={timeList[0].end.format()}/>
                   </Tab>
                   <Tab name="上月">
-                    list 2
+                  <ItemSummary startDate={timeList[1].start.format()} endDate={timeList[1].end.format()}/>
                   </Tab>
                   <Tab name="今年">
-                    list 3
+                    <ItemSummary startDate={timeList[2].start.format()} endDate={timeList[2].end.format()}/>
                   </Tab>
                   <Tab name="自定义时间">
-                    list 4
+                    <ItemSummary startDate={customTime.start} endDate={customTime.end} />
                   </Tab>
                 </Tabs>
-              )
+                <Overlay show={refOverlayVisible.value} class={s.overlay} >
+                  <div class={s.overlay_inner}>
+                    <header>
+                      请选择时间
+                    </header>
+                    <main>
+                          <div class={s.actions}>
+                            <button type="button">取消</button>
+                            <button type="submit">确认</button>
+                          </div>
+                    </main>
+                  </div>
+                </Overlay>
+              </>)
             }
           }</MainLayout>
         )

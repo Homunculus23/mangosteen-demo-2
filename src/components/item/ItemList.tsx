9a7af42098d08127439c1ab63d28143872618572
fpatr <fpatr@qq.com>
2022-11-03 17:42:40 +0800

    初步完成自定义时间弹出框

--- src/components/item/ItemList.tsx
+++ src/components/item/ItemList.tsx
@@ -1,6 +1,7 @@
 import { Overlay } from "vant";
 import { defineComponent, PropType, reactive, ref, watchEffect } from "vue";
 import { MainLayout } from "../../layouts/MainLayout";
+import { Form, FormItem } from "../../shared/Form";
 import { Icon } from "../../shared/Icon";
 import { Tab, Tabs } from "../../shared/Tabs";
 import { Time } from "../../shared/time";
@@ -27,6 +28,7 @@ export const ItemList = defineComponent({
         })
         const refOverlayVisible = ref(false)
         const onSubmitCustomTime = (e: Event) => {
+          //阻止默认动作必须写在最前面，否则可能出bug
           e.preventDefault()
           refOverlayVisible.value = false
         }
@@ -56,10 +58,16 @@ export const ItemList = defineComponent({
                       请选择时间
                     </header>
                     <main>
+                      <Form onSubmit={onSubmitCustomTime}>
+                        <FormItem label='开始时间' v-model={customTime.start} type='date' />
+                        <FormItem label='结束时间' v-model={customTime.end} type='date' />
+                        <FormItem>
                           <div class={s.actions}>
                             <button type="button">取消</button>
                             <button type="submit">确认</button>
                           </div>
+                        </FormItem>
+                      </Form>
                     </main>
                   </div>
                 </Overlay>

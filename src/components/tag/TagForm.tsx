b98994d98432eb5c55c7425bbd87302d5860ae28
fpatr <fpatr@qq.com>
2022-11-03 17:07:33 +0800

    重构 TagForm

--- src/components/tag/TagForm.tsx
+++ src/components/tag/TagForm.tsx
@@ -1,6 +1,7 @@
 import { defineComponent, reactive } from "vue";
 import { Button } from "../../shared/Button";
 import { EmojiSelect } from "../../shared/EmojiSelect";
+import { Form, FormItem } from "../../shared/Form";
 import { Rules, validate } from "../../shared/validate";
 import s from './Tag.module.scss';
 export const TagForm = defineComponent({
@@ -35,38 +36,17 @@ export const TagForm = defineComponent({
             e.preventDefault()
         }
         return () => (
-            <form class={s.form} onSubmit={onSubmit}>
-                {formData.name}
-                <div class={s.formRow}>
-                    <label class={s.formLabel}>
-                        <span class={s.formItem_name}>标签名</span>
-                        <div class={s.formItem_value}>
-                            <input v-model={formData.name} class={[s.formItem, s.input, s.error]}></input>
-                        </div>
-                        <div class={s.formItem_errorHint}>
-                            <span>{errors['name'] ? errors['name']?.[0] : '\u3000'}</span>
-                        </div>
-                    </label>
-                </div>
-                <div class={s.formRow}>
-                    <label class={s.formLabel}>
-                        {/* formData.sign 由 onClickEmoji 传递给 v-model */}
-                        <span class={s.formLabel}>符号 {formData.sign}</span>
-                        <div class={s.formItem_value}>
-                            <EmojiSelect v-model={formData.sign} class={[s.formItem, s.emojiList, s.error]} />
-                        </div>
-                        <div class={s.formItem_errorHint}>
-                            <span>{errors['sign']?.[0]}&#x3000;</span>
-                        </div>
-                    </label>
-                </div>
-                <p class={s.tips}>记账时长按标签即可进行编辑</p>
-                <div class={s.formRow}>    
-                    <div class={s.formItem_value}>
-                        <Button class={[s.formItem, s.button]}>确定</Button>    
-                    </div>
-                </div>
-            </form>
+            <Form onSubmit={onSubmit}>
+                <FormItem label="标签名" type="text" v-model={formData.name} error={errors['name'] ? errors['name']?.[0] : '\u3000'}/>
+                <FormItem label={'符号' + formData.sign} type="emojiSelect" v-model={formData.sign} error={errors['sign'] ? errors['sign']?.[0] : '\u3000'}/>
+                <FormItem>
+                    {/* FormItem可以为 p 和 Button 自动添加margin。 自定义内容不需要写type。*/}
+                    <p class={s.tips}>记账时长按标签即可进行编辑</p>
+                </FormItem>
+                <FormItem>
+                    <Button class={[s.button]}>确定</Button>
+                </FormItem>
+            </Form>
         );
     }
 })

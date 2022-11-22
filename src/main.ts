d2075ac07bfbe179b25328548a1f67744184816a
fpatr <fpatr@qq.com>
2022-11-22 18:21:06 +0800

    优化了 main.ts 的路由守卫，但是代码行数变多了

--- src/main.ts
+++ src/main.ts
@@ -12,22 +12,32 @@ const router = createRouter({ history, routes })
 
 meFetch()
 
+const openList: Record<string, 'exact' | 'startsWith'> = {
+    '/': 'exact', // key有斜杠必须加''
+    '/start': 'exact',
+    '/welcome': 'startsWith',
+    '/sign_in': 'startsWith',
+}
+// openList 可优化为：
+// const openList = {
+//     exact: ['/', '/start'],
+//     startsWith: ['/welcome', '/sign_in']
+// }
+
 // 路由守卫
-router.beforeEach(async (to, from)=>{
-    // 如果路由为'/' ，或者以 '/welcome' 或 'sign_in' 开头(startWith的作用)
-    if(to.path === '/' || to.path === '/start' || to.path.startsWith('/welcome') || to.path.startsWith('/sign_in')){
-        return true
-    }else{
-    // 请求当前用户信息，如果成功就进入 ItemList/ItemCreate，否则进入登录页面
-      const path = await mePromise!.then(
-        // 成功
-        () => true,
-        // 失败
-        () =>{
-        return '/sign_in?return_to=' + to.path
-      })
-      return path
+router.beforeEach((to, from)=>{
+    for(const key in openList){
+        const value = openList[key]
+        // 若 路由 === ('exact'的 key) ，或以 ('startsWith' 的 key) 开头，返回 true
+        if((value === 'exact' && to.path === key) || (value === 'startsWith' && to.path.startsWith(key))){
+            return true
+        } 
     }
+    // 请求当前用户信息，如果成功就进入 ItemList/ItemCreate，否则进入登录页面
+    return mePromise!.then(
+        () => true,// 成功
+        () => '/sign_in?return_to=' + to.path// 失败
+    )
 })
 
 const app = createApp(App)

6cf8f6b781b9d1b381ece3d406ffa624d0121fba
fpatr <fpatr@qq.com>
2022-11-30 16:43:58 +0800

    触发长按效果

--- src/components/item/Tags.tsx
+++ src/components/item/Tags.tsx
@@ -1,4 +1,4 @@
-import { defineComponent, onUpdated, PropType } from "vue";
+import { defineComponent, onUpdated, PropType, ref } from "vue";
 import { Button } from "../../shared/Button";
 import { http } from "../../shared/Http";
 import { Icon } from "../../shared/Icon";
@@ -26,9 +26,36 @@ export const Tags = defineComponent({
     const onSelect = (tag: Tag) => {
       context.emit("update:selected", tag.id);
     };
+    // 实现长按标签且未移开手指时，编辑该标签。监听手指移动的 onTouchMove 必须放在外面的 div 上，否则无法监听移动。
+    const timer = ref<number>();
+    const currentTag = ref<HTMLDivElement>();
+    const onLongPress = () => {
+      console.log("长按");
+    };
+    const onTouchStart = (e: TouchEvent) => {
+      currentTag.value = e.currentTarget as HTMLDivElement;
+      timer.value = setTimeout(() => {
+        onLongPress();
+      }, 500);
+    };
+    const onTouchEnd = (e: TouchEvent) => {
+      clearTimeout(timer.value);
+    };
+    const onTouchMove = (e: TouchEvent) => {
+      const pointedElement = document.elementFromPoint(
+        e.touches[0].clientX,
+        e.touches[0].clientY
+      );
+      if (
+        !currentTag.value?.contains(pointedElement) &&
+        currentTag.value !== pointedElement
+      ) {
+        clearTimeout(timer.value);
+      }
+    };
     return () => (
       <>
-        <div class={s.tags_wrapper}>
+        <div class={s.tags_wrapper} onTouchmove={onTouchMove}>
           <RouterLink to={`/tags/create?kind=${props.kind}`} class={s.tag}>
             <div class={s.sign}>
               <Icon name="add" class={s.createTag} />
@@ -40,6 +67,9 @@ export const Tags = defineComponent({
             <div
               class={[s.tag, props.selected === tag.id ? s.selected : ""]}
               onClick={() => onSelect(tag)}
+              // 长按编辑，监听长按时间
+              onTouchstart={onTouchStart}
+              onTouchend={onTouchEnd}
             >
               <div class={s.sign}>{tag.sign}</div>
               <div class={s.name}>{tag.name}</div>

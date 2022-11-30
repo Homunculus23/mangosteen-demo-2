5be9ccdb3d3b24cf96008996c9a0c0fb551695ec
fpatr <fpatr@qq.com>
2022-11-30 10:47:26 +0800

    安装 prettier 进行格式化

--- src/components/item/ItemCreate.tsx
+++ src/components/item/ItemCreate.tsx
@@ -12,7 +12,7 @@ import { useRouter } from "vue-router";
 import { Dialog } from "vant";
 import { AxiosError } from "axios";
 export const ItemCreate = defineComponent({
-    props:{
+    props: {
         name: {
             type: String as PropType<string>
         }
@@ -26,9 +26,9 @@ export const ItemCreate = defineComponent({
         })
         const router = useRouter()
         const onError = (error: AxiosError<ResourceError>) => {
-            if(error.response?.status === 422){
+            if (error.response?.status === 422) {
                 Dialog.alert({
-                    title:'出错',
+                    title: '出错',
                     message: Object.values(error.response.data.errors).join('\n')
                 })
             }
@@ -36,32 +36,32 @@ export const ItemCreate = defineComponent({
         }
         const onSubmit = async () => {
             const response = await http.post<Resource<Item>>('/items', formData,
-            { params: {_mock:'itemCreate'} }).catch(onError)
+                { params: { _mock: 'itemCreate' } }).catch(onError)
             router.push('/items')
         }
         // 本页面的接口，即使后端做出来了也不会马上有数据，我们终究要 Mock 一波假数据
         return () => (
             //注意：<MainLayout> 和上下花括号之间不能有空格，否则花括号中的内容将会被视为数组，无法变成插槽。
             <MainLayout class={s.layout}>{{
-                icon: () => <Icon name="left" class={s.navIcon}/>,
+                icon: () => <Icon name="left" class={s.navIcon} />,
                 title: () => '记一笔',
                 default: () => <>
-                <div class={s.wrapper}>
+                    <div class={s.wrapper}>
                         {/* 网页第一次渲染时就将refKind.value赋予 update: 的 selected。
                         双向绑定事件，当回调参数与refKind.value不同时，将获取的回调参数赋予refKind.value，并将refKind.value赋予 update: 的 selected，该行为将使Tabs重新渲染。 */}
                         <Tabs v-model:selected={formData.kind} class={s.tabs} >
                             {/* Tab 内组件名均为Tags，Vue 在此时会出现切换不刷新的问题（动画也有类似问题），此处解决办法是用 v-show 展示所有 Tab 并控制显示/隐藏。该逻辑在 Tabs 组件 */}
                             <Tab name="支出">
-                                <Tags kind='expenses' v-model:selected={formData.tags_id[0]}/>
+                                <Tags kind='expenses' v-model:selected={formData.tags_id[0]} />
                             </Tab>
                             <Tab name="收入">
-                                <Tags kind='income' v-model:selected={formData.tags_id[0]}/>
+                                <Tags kind='income' v-model:selected={formData.tags_id[0]} />
                             </Tab>
                         </Tabs>
                         {/* 用div为InputPad作定位 */}
                         <div class={s.inputPad_wrapper}>
-                            <InputPad 
-                                v-model:happenAt={formData.happen_at} 
+                            <InputPad
+                                v-model:happenAt={formData.happen_at}
                                 v-model:amount={formData.amount}
                                 onSubmit={onSubmit}
                             />
--- src/env.d.ts
+++ src/env.d.ts
@@ -1,22 +1,28 @@
 /// <reference types="vite/client" />
 
-declare module '*.vue' {
-  import type { DefineComponent } from 'vue'
+declare module "*.vue" {
+  import type { DefineComponent } from "vue";
   // eslint-disable-next-line @typescript-eslint/no-explicit-any, @typescript-eslint/ban-types
-  const component: DefineComponent<{}, {}, any>
-  export default component
+  const component: DefineComponent<{}, {}, any>;
+  export default component;
 }
 
 // 全局引用声明
-type JSONValue = null | boolean | string | number | JSONValue[] | Record<string, JSONValue>
+type JSONValue =
+  | null
+  | boolean
+  | string
+  | number
+  | JSONValue[]
+  | Record<string, JSONValue>;
 
 type Tag = {
-  id: number,
-  user_id: number,
-  name: string,
-  sign: string,
-  kind: 'expenses' | 'income'
-}
+  id: number;
+  user_id: number;
+  name: string;
+  sign: string;
+  kind: "expenses" | "income";
+};
 
 // Item 的类型可以看后端文档，或者直接问
 type Item = {
@@ -25,22 +31,22 @@ type Item = {
   amount: number;
   tags_id: number[];
   happen_at: string;
-  kind: 'expenses' | 'income';
-}
+  kind: "expenses" | "income";
+};
 
 type Resources<T = any> = {
-  resources: T[],
+  resources: T[];
   pager: {
-    page: number,// 如果后端不靠谱也没法沟通, page偶尔传空, 只能在number后面加上 | null ,后面用到page的地方再做判断
-    per_page: number,
-    count: number
-  }
-}
+    page: number; // 如果后端不靠谱也没法沟通, page偶尔传空, 只能在number后面加上 | null ,后面用到page的地方再做判断
+    per_page: number;
+    count: number;
+  };
+};
 
 type Resource<T> = {
-  resource: T
-}
+  resource: T;
+};
 
 type ResourceError = {
-  errors: Record<string, string[]>
-}
+  errors: Record<string, string[]>;
+};

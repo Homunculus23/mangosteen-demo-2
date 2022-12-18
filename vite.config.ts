d577b74600a55dc1803779c9ff6fd2702126d6ef
fpatr <fpatr@qq.com>
2022-12-18 19:25:30 +0800

    删除 base

--- vite.config.ts
+++ vite.config.ts
@@ -1,36 +1,37 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-import vueJsx from '@vitejs/plugin-vue-jsx'
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+import vueJsx from "@vitejs/plugin-vue-jsx";
 // @ts-nocheck
-import { svgstore } from './src/vite_plugins/svgstore';
-import styleImport, {VantResolve} from 'vite-plugin-style-import';
+import { svgstore } from "./src/vite_plugins/svgstore";
+import styleImport, { VantResolve } from "vite-plugin-style-import";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base:'http://blue-cat-expense-pro.oss-cn-heyuan.aliyuncs.com',
   plugins: [
     vue(),
     vueJsx({
       transformOn: true,
-      mergeProps: true
+      mergeProps: true,
     }),
     svgstore(),
     //Vant UI 插件
     styleImport({
-      resolves:[VantResolve()],
+      resolves: [VantResolve()],
       //vant默认路径错误，添加解析文件
-      libs: [{
-        libraryName: 'vant',
-        esModule: true,
-        resolveStyle: name => `../es/${name}/style`
-      }]
+      libs: [
+        {
+          libraryName: "vant",
+          esModule: true,
+          resolveStyle: (name) => `../es/${name}/style`,
+        },
+      ],
     }),
   ],
   server: {
     proxy: {
-      '/api/v1': {
-        target: 'http://121.196.236.94:3000/',
-      }
-    }
+      "/api/v1": {
+        target: "http://121.196.236.94:3000/",
+      },
+    },
   },
-})
+});
